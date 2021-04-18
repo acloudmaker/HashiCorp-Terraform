2d4a2da71b0b380f5de44d9ec15cc00ec155622c
JDL <jdluther2020@gmail.com>
2021-04-17 18:41:29 -0700

    Modified BACKENDS_LocalOp_RemoteState main.tf

--- AWS/BACKENDS_LocalOp_RemoteState/main.tf
+++ AWS/BACKENDS_LocalOp_RemoteState/main.tf
@@ -5,11 +5,17 @@
 # 1. Create a CLI-driven Terraform Cloud workspace
 # 2. Navigate to the workspace's settings and under general, change the execution mode on the workspace to be "local"
 # Then just follow the instructions on the DocRef. The following are FYI key learning points.
-# https://app.terraform.io/app/settings/tokens?source=terraform-login
-# Terraform will store the token in plain text in the following file for use by subsequent commands: ~/.terraform.d/credentials.tfrc.json
-# Navigate to the state file to see the state stored @ https://app.terraform.io/app/ORG-NAME/workspaces/WORKSPACE-NAME/states/STATE_FILE-NAME
+# - https://app.terraform.io/ : To login directly 
+# - https://app.terraform.io/app/settings/tokens?source=terraform-login : This will prompt to create a token. Cancel is you already have a token
+# - Terraform will store the token in plain text in the following file for use by subsequent commands: ~/.terraform.d/credentials.tfrc.json
+# - Navigate to the state file to see the state stored @ https://app.terraform.io/app/ORG-NAME/workspaces/WORKSPACE-NAME/states/STATE_FILE-NAME
+# - Toekn cleanup: terraform logout [hostname (default app.terraform.io)] will remove credentials stored by terraform login
+# - https://www.terraform.io/docs/internals/credentials-helpers.html : Use credentials helpers as an alternative approach to customize credentials using an external program
+
+If you don't provide an explicit hostname, Terraform will assume you want to log out of Terraform Cloud at app.terraform.io
 
 terraform {
+  # The block below canbe copied from https://app.terraform.io/app/ORG-NAME/workspaces/WORKSPACE-NAME
   backend "remote" {
     organization = "ORG-NAME"
 
